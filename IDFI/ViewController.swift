143b8a07da758faca46623a659ae0705c5f8f9ad
IvánMS <IvanMS@IPNSVR2014BJV.local>
2018-06-24 01:49:52 -0500

    Proyecto listo para iniciar el login

---
+++ IDFI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IDFI
+//
+//  Created by IvánMS on 24/06/18.
+//  Copyright © 2018 ivanSo3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
