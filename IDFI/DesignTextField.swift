38ffec1e9184e56f9bb34042f005445dd2460b66
IvánMS <IvanMS@IPNSVR2014BJV.local>
2018-06-28 17:02:35 -0500

    Se colocaron estilos a los textField

---
+++ IDFI/DesignTextField.swift
@@ -0,0 +1,21 @@
+//
+//  DesignTextField.swift
+//  IDFI
+//
+//  Created by IvánMS on 28/06/18.
+//  Copyright © 2018 ivanSo3. All rights reserved.
+//
+
+import UIKit
+
+class DesignTextField: UITextField {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
