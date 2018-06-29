d5d9d0508ef64e66df8a3863713c76c84149afa1
IvánMS <IvanMS@IPNSVR2014BJV.local>
2018-06-29 01:44:05 -0500

    Se comenzó la validación de la autenticación

--- IDFI/LoginViewController.swift
+++ IDFI/LoginViewController.swift
@@ -8,18 +8,37 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var emailTextField: DesignTextField!
+    @IBOutlet weak var passwordTextField: DesignTextField!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    /* Se oculta el teclado cada que se presione enter */
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    @IBAction func loginPressed(_ sender: Any) {
+        if let email = self.emailTextField.text,
+            let password = self.passwordTextField.text,
+            (!email.isEmpty && !password.isEmpty){
+            AuthService.shared.loginUser(email: email, password: password)
+        }else{
+            let alert = UIAlertController(title: "Usuario y contraseña incorrectos", message: "Ingrese usuario y contraseña para continuar", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
+            present(alert,animated: true)
+        }
+    }
 
-
+    
 }
 
