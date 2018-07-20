bb07a4eae68ca44b52195e1e1060ed484ef6619e
IvánMS <IvanMS@IPNSVR2014BJV.local>
2018-07-20 04:16:25 -0500

    Se agregó tab bar controller y controladores

---
+++ IDFI/view/StudentVouchersTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  StudentVouchersTableViewCell.swift
+//  IDFI
+//
+//  Created by IvánMS on 20/07/18.
+//  Copyright © 2018 ivanSo3. All rights reserved.
+//
+
+import UIKit
+
+class StudentVouchersTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
